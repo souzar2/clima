0494c46de75c07ed12c832cc21709ad6fb73440e
souzar2 <arthursouzacomp@gmail.com>
2023-09-21 11:02:55 -0300

    mensagem de erro adicionada

--- script.js
+++ script.js
@@ -2,61 +2,65 @@ const key = "0b316680f6559d29a6872528b1fe1ef6"
 
 
 function showData(data) {
-    document.querySelector(".city").innerHTML = "Tempo em " + data.name
-    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "° C"
-    document.querySelector(".text").innerHTML = data.weather[0].description
-    document.querySelector(".hum").innerHTML = "Umidade: " + data.main.humidity + "%"
-    document.querySelector(".img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-    console.log(data)
+        try{
+        document.querySelector(".city").innerHTML = "Tempo em " + data.name
+        document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "° C"
+        document.querySelector(".text").innerHTML = data.weather[0].description
+        document.querySelector(".hum").innerHTML = "Umidade: " + data.main.humidity + "%"
+        document.querySelector(".img").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+        console.log(data)
 
-    if (data.weather[0].id === 800) {
-        // Obtenha o elemento <body>
-        var bodyElement = document.querySelector("body");
-        bodyElement.style.backgroundImage = "url('imgs/sun.jpg')";
-        bodyElement.style.backgroundSize = "cover";
-        bodyElement.style.backgroundRepeat = "no-repeat";
-    } else {
-        if (data.weather[0].id > 199 && data.weather[0].id < 233) {
+        if (data.weather[0].id === 800) {
+            // Obtenha o elemento <body>
             var bodyElement = document.querySelector("body");
-            bodyElement.style.backgroundImage = "url('imgs/thunder.jpg')";
+            bodyElement.style.backgroundImage = "url('imgs/sun.jpg')";
             bodyElement.style.backgroundSize = "cover";
             bodyElement.style.backgroundRepeat = "no-repeat";
         } else {
-            if (data.weather[0].id > 299 && data.weather[0].id < 322) {
+            if (data.weather[0].id > 199 && data.weather[0].id < 233) {
                 var bodyElement = document.querySelector("body");
-                bodyElement.style.backgroundImage = "url('imgs/drizzle.jpg')";
+                bodyElement.style.backgroundImage = "url('imgs/thunder.jpg')";
                 bodyElement.style.backgroundSize = "cover";
                 bodyElement.style.backgroundRepeat = "no-repeat";
             } else {
-                if (data.weather[0].id >= 500 && data.weather[0].id < 532) {
+                if (data.weather[0].id > 299 && data.weather[0].id < 322) {
                     var bodyElement = document.querySelector("body");
-                    bodyElement.style.backgroundImage = "url('imgs/rain.jpg')";
+                    bodyElement.style.backgroundImage = "url('imgs/drizzle.jpg')";
                     bodyElement.style.backgroundSize = "cover";
                     bodyElement.style.backgroundRepeat = "no-repeat";
                 } else {
-                    if (data.weather[0].id > 700 && data.weather[0].id < 782) {
+                    if (data.weather[0].id >= 500 && data.weather[0].id < 532) {
                         var bodyElement = document.querySelector("body");
-                        bodyElement.style.backgroundImage = "url('imgs/misty.jpg')";
+                        bodyElement.style.backgroundImage = "url('imgs/rain.jpg')";
                         bodyElement.style.backgroundSize = "cover";
                         bodyElement.style.backgroundRepeat = "no-repeat";
                     } else {
-                        if (data.weather[0].id >= 600 && data.weather[0].id < 623) {
+                        if (data.weather[0].id > 700 && data.weather[0].id < 782) {
                             var bodyElement = document.querySelector("body");
-                            bodyElement.style.backgroundImage = "url('imgs/snow.jpg')";
+                            bodyElement.style.backgroundImage = "url('imgs/misty.jpg')";
                             bodyElement.style.backgroundSize = "cover";
                             bodyElement.style.backgroundRepeat = "no-repeat";
                         } else {
-                            if (data.weather[0].id > 800 && data.weather[0].id < 805) {
+                            if (data.weather[0].id >= 600 && data.weather[0].id < 623) {
                                 var bodyElement = document.querySelector("body");
-                                bodyElement.style.backgroundImage = "url('imgs/clouds.jpg')";
+                                bodyElement.style.backgroundImage = "url('imgs/snow.jpg')";
                                 bodyElement.style.backgroundSize = "cover";
                                 bodyElement.style.backgroundRepeat = "no-repeat";
+                            } else {
+                                if (data.weather[0].id > 800 && data.weather[0].id < 805) {
+                                    var bodyElement = document.querySelector("body");
+                                    bodyElement.style.backgroundImage = "url('imgs/clouds.jpg')";
+                                    bodyElement.style.backgroundSize = "cover";
+                                    bodyElement.style.backgroundRepeat = "no-repeat";
+                                }
                             }
                         }
                     }
                 }
             }
         }
+    }catch{
+        document.querySelector(".city").innerHTML = "Cidade não encontrada, tente novamente"
     }
 }
 
